66bde9a551d151477a20346627055715cd851c8e
mujinlu <mujinlu@gmail.com>
2017-01-06 15:31:29 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,61 @@
+var result="";
+function decrypt(num,str){
+	var code = parseInt(num);
+    var temp = [];
+    	interpreter = ['A', 'B', 'C', 'D', 'E', 'F', 'G',
+			'H', 'I', 'J', 'K', 'L', 'M', 'N',
+			'O', 'P', 'Q', 'R', 'S', 'T',
+			'U', 'V', 'W', 'X', 'Y', 'Z'];
+    if (!str) {
+         return "Please input something";
+    }
+    for (var i = 0; i < str.length; i++) {
+        if (str[i] === ' ') {
+            temp.push(' ');
+            continue;
+        }
+        temp.push(interpreter[(str.charCodeAt(i) - code - 65 + 26) % 26]);
+    }
+    var result = temp.join("");
+    result = result.toLocaleLowerCase();
+    return result;
+}
+function encrypt(num,str){
+	var i = 0,en_arr = [];
+			// 'a' code is 97；'A' code is 65
+			for(i = 0; i < 26; i++ ){
+				en_arr.push(String.fromCharCode(65 + i));
+			}
+			for(i = 0; i < num; i++){
+				en_arr.push(en_arr.shift());
+			}
+			// 要加密的文本%%
+	        var str = str;
+	        var temp = [];
+	        var match = /^[A-Za-z\s]+$/;
+	        if (str.match(match)) {
+	            str = str.toLocaleUpperCase();
+	            for (var i = 0; i < str.length; i++) {
+	                if (str[i] === ' ') {
+	                    temp.push(' ');
+	                    continue;
+	                }
+	                temp.push(en_arr[str.charCodeAt(i) - 65]);
+	            }
+	            return temp.join("");
+            };
+            return "Input not valid";
+}
+$(".encrypt").click(function(){
+	var code = $(".num").val();
+	var str = $(".str").val();
+	result = encrypt(code,str);
+	$(".show").text(result);
+});
+$(".decrypt").click(function(){
+	//解密
+	var code = $(".num").val();
+	var str = $(".str").val();
+	result = decrypt(code,str);
+	$(".show").text(result);
+});
